2965803b3d1ee569f1adcdce7f293c69a46d7fbc
Avery Behm <darthavery23@gmail.com>
2022-09-04 00:17:58 -0600

    commit tree 9f05f33ea3bf205585ca75a4c0b5ef0cf24674bd

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+const employee = {
+    name: "Maxine",
+    streetAddress: "42 Starlake Street"
+}
+function updateEmployeeWithKeyAndValue(
+    employee,
+    key,
+    value
+) {
+return {
+    ...employee,
+    name: "Sam",
+    streetAddress: "11 Broadway"
+}; }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    key,
+    value
+) {
+    employee.streetAddress = "12 Broadway";
+return employee;
+ }
+
+ function deleteFromEmployeeByKey(
+    employee,
+    key
+ ) {
+    const newEmployee = {...employee};
+    newEmployee[key] = undefined;
+    return newEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(
+    employee,
+    key
+ ) {
+    employee[key] = undefined;
+    return employee;
+ }
